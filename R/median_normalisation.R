d48024be7c3f88b71efbe5df105b608c1fde0420
jpquast <janphilipp.quast@gmail.com>
2020-02-25 14:54:13 +0100

    Updated median_normalisation function

--- R/median_normalisation.R
+++ R/median_normalisation.R
@@ -2,9 +2,9 @@
 #'
 #' Median normalise intensity values for all samples of mass spectrometry data. The intensity values are ideally log2 transformed. The output is a new column called normalised_intensity_log2.
 #'
-#' @param data A data frame containing at least sample names and intensity values.
-#' @param file_column_name The name of the column containing the sample names.
-#' @param intensity_column_name The name of the column containing the intensity values.
+#' @param .data A data frame containing at least sample names and intensity values.
+#' @param .file_column_name The name of the column containing the sample names.
+#' @param .intensity_column_name The name of the column containing the intensity values.
 #' @param na.rm A logical indicating wther missing values should be removed.
 #'
 #' @import tidyverse
@@ -17,12 +17,12 @@
 #' median_normalisation(data, sample_name, intensity_log2, na.rm = TRUE)
 #' }
 median_normalisation <-
-function(data, file_column_name, intensity_column_name, na.rm = FALSE){
-  data %>%
-    group_by({{file_column_name}})%>%
-    mutate(median_run_intensity = median({{intensity_column_name}}, na.rm = na.rm))%>%
+function(.data, .file_column_name, .intensity_column_name, na.rm = FALSE){
+  .data %>%
+    group_by({{.file_column_name}})%>%
+    mutate(median_run_intensity = median({{.intensity_column_name}}, na.rm = na.rm))%>%
     ungroup()%>%
     mutate(median_intensity = median(unique(median_run_intensity), na.rm = na.rm))%>%
-    mutate(normalised_intensity_log2 = {{intensity_column_name}}/median_run_intensity*median_intensity)%>%
+    mutate(normalised_intensity_log2 = {{.intensity_column_name}}/median_run_intensity*median_intensity)%>%
     select(- median_run_intensity, -median_intensity)
 }
